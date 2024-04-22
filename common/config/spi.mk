7eb26aeb1e55b1f350a88392566af8a355b5529f
khoa.dang <khoa.dang@cavliwireless.com>
2024-04-22 10:54:40 +0000

    build:: Update

--- common/config/spi.mk
+++ common/config/spi.mk
@@ -1,10 +1,14 @@
 PRODUCT_PACKAGES += vendor.cavli.hardware.spi@1.0 \
-					vendor.cavli.hardware.spi@1.0-service \
+                    vendor.cavli.hardware.spi@1.0-service \
 
 
 # Add cavli manifest
-# DEVICE_MANIFEST_FILE += vendor/cavli/device/manifest.xml
-DEVICE_MATRIX_FILE += vendor/cavli/device/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += vendor/common/device/manifest.xml
+
+# Set value for DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE didn't work. I work arround by eit
+# vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml. I should find other
+# better way
+# DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/common/device/framework_manifest.xml
 
 # Add cavli seplicy
-BOARD_SEPOLICY_DIRS += vendor/cavli/sepolicy
+BOARD_SEPOLICY_DIRS += vendor/common/sepolicy
