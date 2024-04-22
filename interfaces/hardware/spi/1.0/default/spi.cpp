8ba7e31e08c48d050a6c0928049293dea6845898
khoa.dang <khoa.dang@cavliwireless.com>
2024-04-22 22:04:32 +0700

    demo:: Update client and jav lib

--- interfaces/hardware/spi/1.0/default/spi.cpp
+++ interfaces/hardware/spi/1.0/default/spi.cpp
@@ -1,4 +1,4 @@
-#define LOG_TAG "vendor.cavli.cavli_spi@1.0-impl"
+#define LOG_TAG "vendor.cavli.hardware.spi@1.0-impl"
 
 #include <log/log.h>
 #include <hardware/hardware.h>
@@ -14,6 +14,7 @@ namespace implementation {
 
 Return<bool> Cavspi::sendData(const hidl_vec<uint8_t>& data){
     (void)data;
+    ALOGI("sendData length=%zu", data.size());
     return true;
 
 };
@@ -21,15 +22,18 @@ Return<bool> Cavspi::sendData(const hidl_vec<uint8_t>& data){
 Return<int32_t> Cavspi::readData(const hidl_vec<uint8_t>& data, uint32_t length){
     (void)length;
     (void)data;
+    ALOGI("readData length=%i", length);
     return 0;
 };
 
 Return<bool> Cavspi::openConnection(const hidl_string& device){
     (void)device;
+    ALOGI("openConnection device=%s", device.c_str());
     return true;
 };
 
 Return<bool> Cavspi::closeConnection(){
+    ALOGI("closeConnection");
     return true;
 };
 
