c506cc0b6e96d3ea8e226b86487df346e03e2685
Do Dang Khoa <khoa.dang@cavliwireless.com>
2024-05-19 11:23:55 +0700

    Update demo nesh

--- common/config/qssi_cavlidemo.mk
+++ common/config/qssi_cavlidemo.mk
@@ -5,3 +5,7 @@ PRODUCT_PACKAGES += vendor.cavli.hardware.spi@1.0 \
 PRODUCT_PACKAGES += vendor.cavli.hardware.uart@1.0 \
                     picocom \
                     cavliuartdemo
+
+# Include package pre-build
+PRODUCT_PACKAGES += cavliuartdemoprebuild
+
